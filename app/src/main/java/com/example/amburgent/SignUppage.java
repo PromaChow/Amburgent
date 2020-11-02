ad84104369882e1f744129e62320b5c422d41226
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2020-11-03 01:20:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/amburgent/SignUppage.java
@@ -0,0 +1,37 @@
+package com.example.amburgent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignUppage extends AppCompatActivity {
+
+    EditText user,phone;
+    Button withFb,withGoog,sign_in;
+    String phoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_uppage);
+        user = findViewById(R.id.username);
+        phone = findViewById(R.id.phone);
+        withFb =findViewById(R.id.fb_sign);
+        withGoog = findViewById(R.id.google_sign);
+
+    }
+    public void sign_in(View v){
+        phoneNumber = phone.getText().toString();
+
+        Intent intent = new Intent(getApplicationContext(),otp.class);
+        intent.putExtra("phoneNumber",phoneNumber);
+        startActivity(intent);
+    }
+
+
+
+}

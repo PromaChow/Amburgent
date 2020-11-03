943fb72653f30edd5873651fbadeca31371e5ac7
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2020-11-04 02:00:11 +0600

    hello

--- app/src/main/java/com/example/amburgent/MainActivity2.java
+++ app/src/main/java/com/example/amburgent/MainActivity2.java
@@ -2,9 +2,12 @@ package com.example.amburgent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,6 +15,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        FirebaseApp.initializeApp(getApplicationContext());
+        //FirebaseApp.initializeApp(getApplicationContext());
+       
+
+    }
+
+    public void log_out(View view) {
+
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getApplicationContext(),SignUppage.class);
+        startActivity(intent);
     }
 }

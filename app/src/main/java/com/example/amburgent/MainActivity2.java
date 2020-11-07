6abad71302acc1734416f5525baff874607b5f3c
Proma Chowdhury <48786787+PromaChow@users.noreply.github.com>
2020-11-08 01:14:30 +0600

    hello
    hello

--- app/src/main/java/com/example/amburgent/MainActivity2.java
+++ app/src/main/java/com/example/amburgent/MainActivity2.java
@@ -1,29 +1,145 @@
 package com.example.amburgent;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
 
-public class MainActivity2 extends AppCompatActivity {
+import java.util.Map;
+
+public class MainActivity2 extends AppCompatActivity implements  PopupMenu.OnMenuItemClickListener {
+
+    NavigationView nav;
+    DrawerLayout drawer;
+    Toolbar toolbar;
+    ImageView imageView;
+    String  imgUri;
+    String code,Id;
+
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db=FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_user_dashboard);
+        Intent intent = getIntent();
+        code = intent.getStringExtra("code");
+        Id = intent.getStringExtra("id");
+        Toast.makeText(getApplicationContext(),code,Toast.LENGTH_LONG).show();
+
+        drawer = findViewById(R.id.drawer_layout);
+        nav = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.tool_bar);
+        View headerView = nav.getHeaderView(0);
+        imageView = headerView.findViewById(R.id.Iv);
+
+
+
+
+
+            DocumentReference docRef = db.collection("Users").document(Id);
+           docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+               @Override
+               public void onSuccess(DocumentSnapshot documentSnapshot) {
+                   imgUri = documentSnapshot.get("PhotoUrl").toString();
+                   Picasso.get().load(imgUri).into(imageView);
+
+               }
+           });
+
+
+
+
+
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar
+                , R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                switch (item.getItemId()) {
+                    case R.id.prof_item:
+                        Toast.makeText(getApplicationContext(), " Profile selected", Toast.LENGTH_LONG).show();
+                    case R.id.trans_item:
+                        Toast.makeText(getApplicationContext(), " Transaction History selected", Toast.LENGTH_LONG).show();
+                    case R.id.rides_item:
+                        Toast.makeText(getApplicationContext(), " Rides selected", Toast.LENGTH_LONG).show();
+
+                }
+                return true;
+            }
+        });
+
         //FirebaseApp.initializeApp(getApplicationContext());
-       
+
 
     }
 
+    public void showPopUp(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popup);
+        popup.show();
+
+    }
+
+
     public void log_out(View view) {
 
+
+
+
+
+//        LoginManager login_manager= LoginManager.getInstance();
+//        login_manager.logOut();
         FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(getApplicationContext(),SignUppage.class);
+        Intent intent = new Intent(getApplicationContext(), SignUppage.class);
         startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.pfpshow_item:
+                Toast.makeText(getApplicationContext(), " Profile selected", Toast.LENGTH_LONG).show();
+            case R.id.pfpupload_item:
+                Toast.makeText(getApplicationContext(), " Transaction History selected", Toast.LENGTH_LONG).show();
+
+
+        }
+        return true;
     }
 }
